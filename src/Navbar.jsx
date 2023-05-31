f33f2d7b3c2eff8b6da8f2ac32136d2e79525d22
vmoraes424 <vmoraes424@gmail.com>
2023-05-31 11:45:15 -0300

    Adicionado avatar de perfil na navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import "./Navbar"
+import "./Navbar.css"
 
 const Navbar = () => {
+
     return (
         <nav>
-            <img src="../img/logo.jpg" alt=""/>
+            <img src="../img/logo.jpg" alt="" width={120}/>
+            <div className="direita">
+                <img src="../img/avatar.png" alt="" width={40}/>
+                ⏷
+            </div>
         </nav>
     );
 }
