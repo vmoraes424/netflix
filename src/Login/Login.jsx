b8a1ca1e6b0a4e07708555399da949c3db3a8801
Vinicius Moraes <vmoraes424@gmail.com>
2023-06-09 18:49:19 -0300

    Levado ao / após login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+  function submit(e) {
+    e.preventDefault();
+    window.location.href = "/";
+  }
   return (
     <>
       <div className="wrapper-bg">
-        <img src="../img/bg.jpg" alt="" id="backdrop"/>
+        <img src="../img/bg.jpg" alt="" id="backdrop" />
         <img src="../img/logobg.png" alt="" width={180} id="login-logo" />
         <main className="login-container">
           <div className="wrapper-form">
-            <form>
+            <form onSubmit={submit}>
               <h2>Entrar</h2>
               <label htmlFor="email">Email ou número de telefone</label>
-              <input type="email" name="email" id="email" />
+              <input type="email" name="email" id="email" required />
               <label htmlFor="senha">Senha</label>
-              <input type="password" name="senha" id="senha" />
-              <input type="button" value="Entrar" />
+              <input type="password" name="senha" id="senha" required />
+              <input type="submit" value="Entrar" />
               <div className="embaixo">
                 <div className="lembre">
                   <input type="checkbox" name="check" id="check" />
